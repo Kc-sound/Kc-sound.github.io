b42bea0e3c08e8a3b2d0b468ad5964b14a6c85bb
forumtesti <48433207+forumtesti@users.noreply.github.com>
2021-05-23 22:03:03 +0200

    3d Guitar POGU!

---
+++ js/guitar.js
@@ -0,0 +1,96 @@
+
+import {GLTFLoader} from './GLTFLoader.js'
+import {OrbitControls} from './OrbitControls.js'
+
+//la scene 3d de three,js
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+
+//lumiere simple
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
+
+// le canvas
+const renderer = new THREE.WebGLRenderer({
+        antialias:true,
+        alpha:true
+})
+let theCanvas = renderer.domElement;
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+theCanvas.classList.add("absolute");
+
+
+//responsive canvas
+window.addEventListener('resize',() =>{
+    renderer.setSize(window.innerWidth,window.innerHeight)
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix()
+})
+
+
+
+
+//la Guitare
+let loader = new GLTFLoader();
+let myobj;
+loader.load( './models/guitar.gltf', function ( gltf ) {
+
+    const box = new THREE.Box3().setFromObject( gltf.scene );
+    const center = box.getCenter( new THREE.Vector3() );
+
+    myobj = gltf;
+
+    gltf.scene.position.x += ( gltf.scene.position.x - center.x );
+    gltf.scene.position.y += ( gltf.scene.position.y - center.y );
+    gltf.scene.position.z += ( gltf.scene.position.z - center.z );
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+//decaler la camera de 70 (px?)
+camera.position.z = 70;
+camera.position.x = -30;
+
+//La rotation de l'element
+function rotateScene(deltaX, deltaY) {
+    myobj.scene.rotation.y = deltaX  ;
+    myobj.scene.rotation.x = deltaY  ;
+} 
+
+function moveScene(moveX) {
+    myobj.scene.position.x = moveX  ;
+} 
+
+let minAngleX = -0.7,
+    maxAngleX = 0.2,
+    minAngleY = -0.2,
+    maxAngleY = 0.2,
+    maxMove = 5,
+    minMove = -5,
+    maxScreenWidth = window.innerWidth,
+    maxScreenHeight = window.innerHeight,
+    csize = document.getElementById("main");
+
+document.addEventListener('mousemove', (e) =>{
+    let deltaX = ((e.clientX) * (maxAngleX-minAngleX)/maxScreenWidth) + minAngleX,
+        deltaY = ((e.clientY) * (maxAngleY-minAngleY)/maxScreenHeight) + minAngleY;
+    let moveX = ((e.clientX) * (maxMove-minMove)/maxScreenWidth) + minMove;
+
+    rotateScene(deltaX,deltaY);
+    moveScene(moveX);
+});
+
+
+const animate = function () {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+};
+
+animate();
